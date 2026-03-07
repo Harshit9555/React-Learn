ce5570d101d8cd1438f15dc4e7ba172b9200184a
Harshit Mishra <harshitmishra8953@gmail.com>
2026-03-07 10:43:26 +0530

    11-form-Handling Added

---
+++ 11-form-Handling/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+    console.log("Submitted form")
+  }
+  return (
+    <div>
+      <form onSubmit={(e)=>{
+        submitHandler(e);
+      }}>
+        <input type="text" placeholder="Enter your Name" />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App
